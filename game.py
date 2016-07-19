41491b09d3bea1ac4d57f7f92b2ce1647905a464
Kinrany <kinrany@yandex.com>
2016-07-19 18:50:24 +0300

    Добавил второго игрока

--- game.py
+++ game.py
@@ -7,14 +7,21 @@ from goalkeeper import Goalkeeper
 pygame.init()
 
 # создаем экран 400х300 и меняем заголовок окна
-display = pygame.display.set_mode((400, 300))
+WIDTH = 400
+HEIGHT = 300
+display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Hello World!')
 
 # цвет фона
 background_color = pygame.Color(127, 198, 127, 0)
 
-# объект игрока
-goalkeeper = Goalkeeper(10, 10, 0, 300)
+# объекты игроков
+goalkeeper1 = Goalkeeper(10, 10, 0, HEIGHT,
+                         color=pygame.Color(255, 64, 64, 0))
+goalkeeper2 = Goalkeeper(WIDTH - 40, 10, 0, HEIGHT,
+                         color=pygame.Color(64, 64, 255, 0),
+                         key_up=pgl.K_w,
+                         key_down=pgl.K_s)
 
 # объект для отслеживания времени
 clock = pygame.time.Clock()
@@ -25,15 +32,17 @@ while True:
         # пользователь закрыл окно
         if event.type == pgl.QUIT:
             pygame.quit()
-            sys.exit()
+#           sys.exit()  # не нужно в интерактивном режиме
 
     # обновление движения игрока
-    goalkeeper.update()
+    goalkeeper1.update()
+    goalkeeper2.update()
 
     # заливка фона
     display.fill(background_color)
     # отрисовка игрока
-    goalkeeper.draw(display)
+    goalkeeper1.draw(display)
+    goalkeeper2.draw(display)
 
     # обновление экрана
     pygame.display.update()
