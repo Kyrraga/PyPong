55051d063cbdfbce37c2a280d44edb757cfbe403
Kyrraga <velin256@gmail.com>
2016-07-19 18:59:02 +0300

    work ples

--- game.py
+++ game.py
@@ -1,36 +1,30 @@
 import pygame, sys
 import pygame.locals as pgl
+from goalkeeper import Goalkeeper
 
 
 # магическая команда инициализации модуля
 pygame.init()
 
-# описание объектов - задний фон и прямоугольник игрока
-WINDOWWIDTH = 500 # размер окна по длине
-WINDOWHEIGHT = 400 # размер окна по ширине
+WINDOWWIDTH = 400 # размер окна по длине
+WINDOWHEIGHT = 300 # размер окна по ширине
 
 RECTWIDTH = round(WINDOWHEIGHT / 100 * 2) + round(WINDOWWIDTH / 100 * 1) # размер игрока по длине
 RECTHEIGHT = round(WINDOWHEIGHT  / 100 * 20) # размер игрока по ширине
 OFFSET = 15 # расстояние игрока от стенки
 
-background_color = pygame.Color(127, 198, 127, 0)
-#goalkeeper_rect = pygame.Rect(OFFSET + RECTWIDTH, 100, OFFSET, 60)
-goalkeeper_rect = pygame.Rect(OFFSET,WINDOWHEIGHT / 2 - RECTHEIGHT,RECTWIDTH,RECTHEIGHT)
-goalkeeper_color = pygame.Color(255, 255, 255, 0)
-
 # создаем экран 400х300 и меняем заголовок окна
 display = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('Pong')
+
+# цвет фона
+background_color = pygame.Color(127, 198, 127, 0)
+
+# объект игрока
+goalkeeper = Goalkeeper(OFFSET, (WINDOWHEIGHT - RECTHEIGHT) / 2, RECTWIDTH, RECTHEIGHT,  0, WINDOWHEIGHT)
 
 # объект для отслеживания времени
 clock = pygame.time.Clock()
-
-# нажата ли клавиша в данный момент
-move_down = False
-move_up = False
-
-# скорость движения игрока
-goalkeeper_speed = 10
 
 while True:
     # перебираем все ивенты в очереди
@@ -39,37 +33,14 @@ while True:
         if event.type == pgl.QUIT:
             pygame.quit()
             sys.exit()
-        # пользователь нажал клавишу
-        elif event.type == pgl.KEYDOWN:
-            if event.key == pgl.K_DOWN:
-                move_down = True
-            if event.key == pgl.K_UP:
-                move_up = True
-        # пользователь отпустил клавишу
-        elif event.type == pgl.KEYUP:
-            if event.key == pgl.K_DOWN:
-                move_down = False
-            if event.key == pgl.K_UP:
-                move_up = False
-
-    #str st = goalkeeper_rect.y
-    pygame.display.set_caption(str(goalkeeper_rect.y))
-
 
     # обновление движения игрока
-    if (goalkeeper_rect.y < WINDOWHEIGHT  - RECTHEIGHT):
-        if move_down:
-            goalkeeper_rect.y += goalkeeper_speed
-    if (goalkeeper_rect.y >= 10):
-        if move_up:
-            goalkeeper_rect.y -= goalkeeper_speed
+    goalkeeper.update()
 
     # заливка фона
     display.fill(background_color)
     # отрисовка игрока
-    pygame.draw.rect(display,
-                     goalkeeper_color,
-                     goalkeeper_rect)
+    goalkeeper.draw(display)
 
     # обновление экрана
     pygame.display.update()
